ae8b91f0d1c1aed56c5aa7107106d6b10dea6998
Rafiqul Islam <rafiquljakir@gmail.com>
2023-12-05 01:54:40 +0600

    Update Main.java

--- Design_Pattern_Final/src/Facade_Exmple_3/Main.java
+++ Design_Pattern_Final/src/Facade_Exmple_3/Main.java
@@ -1,4 +1,13 @@
-
+/*
+/*
+Suppose you want to buy a bag from an online page named “abc” who has three types of bags.
+You talk with the page moderator and order a bag. Page moderator notes customers information, 
+process the order and handles all the works of billing and shipping. You have no idea about the 
+complexities of the ordering system as they are hidden by the page providing page moderator. You 
+just place order to page moderator and get your staff. 
+Now develop the code from the given scenario using appropriate design patterns.
+*/
+*/
 package Facade_Exmple_3;
 
 /**
